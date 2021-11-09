e3d2d10da2c614f0bdad3bec52f1254cfa2c67b3
fanguifang082412 <19958722846@163.com>
2021-11-09 17:50:30 +0800

    第二次提交

--- case/add_team_case.py
+++ case/add_team_case.py
@@ -9,42 +9,24 @@ from untils import DriverUntil
 
 
 
-def get_add_team_data():
-    with open("../data/add_team_data.json", "rb") as f:
+def get_add_team_data(filename, function):
+    with open("../data/%s.json" %filename, "rb") as f:
         list = []
-        data = json.load(f)
+        data = json.load(f)[function]
         for i in data.values():
             list.append((i.get("team_name"), i.get("team_name_all"), i.get("team_image"), i.get("team_builder"), i.get("team_yunying"), i.get("team_office_address"), i.get("team_connecter"), i.get("team_phone"), i.get("team_introduce"), i.get("assert_info")))
     return list
 
-
 #团队运营人员为空数据
-def get_add_team_yunying_is_null():
-    with open("../data/add_team_yunying_is_null.json", "rb") as f:
+def get_team_yunying_is_null(function):
+    with open("../data/add_team_error.json", "rb") as f:
         list = []
-        data = json.load(f)
+        data = json.load(f)[function]
         for i in data.values():
             list.append((i.get("team_name"), i.get("team_name_all"), i.get("team_image"), i.get("team_builder"), i.get("team_office_address"), i.get("team_connecter"), i.get("team_phone"), i.get("team_introduce"), i.get("assert_info")))
     return list
 
-#团队日期为空
-def get_add_team_date_is_null():
-    with open("../data/add_team_date_is_null.json", "rb") as f:
-        list = []
-        data = json.load(f)
-        for i in data.values():
-            list.append((i.get("team_name"), i.get("team_name_all"), i.get("team_image"), i.get("team_builder"), i.get("team_yunying"), i.get("team_office_address"), i.get("team_connecter"), i.get("team_phone"), i.get("team_introduce"), i.get("assert_info")))
-    return list
 
-
-#团队介绍为空
-def get_add_team_service_is_null():
-    with open("../data/add_team_service_is_null.json", "rb") as f:
-        list = []
-        data = json.load(f)
-        for i in data.values():
-            list.append((i.get("team_name"), i.get("team_name_all"), i.get("team_image"), i.get("team_builder"), i.get("team_yunying"), i.get("team_office_address"), i.get("team_connecter"), i.get("team_phone"), i.get("team_introduce"), i.get("assert_info")))
-    return list
 
 
 
@@ -66,8 +48,8 @@ class AddTeamCase(unittest.TestCase):
     def setUp(self) -> None:
         self.driver.get("http://47.102.168.233:8880/")
 
-    @parameterized.expand(get_add_team_data())
-    def test_add_team(self, team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce, assert_info):
+    @parameterized.expand(get_add_team_data("add_team_data", "add_team_error"))
+    def test_add_team_error(self, team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce, assert_info):
         self.add_team_business.add_team(team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce)
         try:
             assert_ele = WebDriverWait(self.driver, 5, 1).until(lambda x: x.find_element(By.XPATH, "/html/body/div[7]/p"))
@@ -76,8 +58,21 @@ class AddTeamCase(unittest.TestCase):
             self.assertEqual(assert_info, assert_ele.text)
         else:
             self.assertEqual(assert_info, assert_ele.text)
-    #
-    # @parameterized.expand(get_add_team_yunying_is_null())
+
+    @parameterized.expand(get_add_team_data("add_team_data", "add_team_success"))
+    def test_add_team_success(self, team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce, assert_info):
+        self.add_team_business.add_team(team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce)
+        try:
+            assert_ele = WebDriverWait(self.driver, 5, 1).until(lambda x: x.find_element(By.XPATH, "/html/body/div[7]/p"))
+        except Exception as e:
+            assert_ele = WebDriverWait(self.driver, 5, 1).until(lambda x: x.find_element(By.XPATH, '//*[@id="app"]/div[3]/div/div[1]/span'))
+            self.assertEqual(assert_info, assert_ele.text)
+        else:
+            self.assertEqual(assert_info, assert_ele.text)
+
+
+
+    # @parameterized.expand(get_team_yunying_is_null("team_yunying"))
     # def test_team_yunying_is_null(self, team_name, team_name_all, image_url, team_builder, team_office, team_connecter, team_phone, team_introduce, assert_info):
     #     self.add_team_business.team_yunying_is_null(team_name, team_name_all, image_url, team_builder, team_office, team_connecter, team_phone, team_introduce)
     #     try:
@@ -88,7 +83,7 @@ class AddTeamCase(unittest.TestCase):
     #     else:
     #         self.assertEqual(assert_info, assert_ele.text)
     #
-    # @parameterized.expand(get_add_team_date_is_null())
+    # @parameterized.expand(get_add_team_data("add_team_data", "add_team_success"))
     # def test_team_date_is_null(self, team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce, assert_info):
     #     self.add_team_business.team_date_is_null(team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce)
     #     try:
@@ -99,7 +94,7 @@ class AddTeamCase(unittest.TestCase):
     #     else:
     #         self.assertEqual(assert_info, assert_ele.text)
     #
-    # @parameterized.expand(get_add_team_service_is_null())
+    # @parameterized.expand(get_add_team_data("add_team_data", "add_team_success"))
     # def test_team_service_is_null(self, team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce, assert_info):
     #     self.add_team_business.team_service_is_null(team_name, team_name_all, image_url, team_builder, team_yunying, team_office, team_connecter, team_phone, team_introduce)
     #     try:
